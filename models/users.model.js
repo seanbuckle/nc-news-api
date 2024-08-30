c64341c704607323e7d8f242bc568d31926174f0
seanbuckle <seanlbuckle@gmail.com>
2024-08-30 15:09:29 +0100

    remove console logs

--- models/users.model.js
+++ models/users.model.js
@@ -8,14 +8,12 @@ exports.selectUsers = () => {
 }
 
 exports.selectUserByUsername = (username) => {
-    console.log("in the model");
     
     const query = format(`SELECT * FROM users WHERE username = %L`, username)
     return db.query(query).then(({ rows }) => {
         if (rows.length < 1){
             return Promise.reject({status: 404, msg: "User not found!"})
         }
-        console.log(rows)
         return rows[0]
     })
 }
