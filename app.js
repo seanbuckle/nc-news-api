ec208c3841fae840b73698b870863b64badbe31d
seanbuckle <seanlbuckle@gmail.com>
2024-09-16 10:07:00 +0100

    add cors for resource sharing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 
 app.use(express.json())
+
+app.use(cors());
 
 const apiRouter = require("./routes/api.routes")
 
